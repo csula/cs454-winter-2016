85e7c6527aecca9aef10fdef634c2f2fa04250ce
Eric Liao <rcliao01@gmail.com>
2016-02-06 09:12:32 -0800

    add BM25 code

---
+++ src/main/java/edu/csula/cs454/retrievalmodel/BM25.java
@@ -0,0 +1,19 @@
+package edu.csula.cs454.retrievalmodel;
+
+/**
+ * A simple class to do Okapi BM25 ranking algorithm
+ */
+public class BM25 {
+    private static final double k1 = 1.2;
+    private static final double k2 = 500;
+    private static final double b = 0.75;
+
+    public static double evaluate(String[] queries, String[] documents) {
+        return 0.0;
+    }
+
+    // use the formula to compute K
+    private static double getK() {
+        return 0.0;
+    }
+}
